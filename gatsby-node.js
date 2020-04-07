c978ef41a48f1d0b14d90f24c6e76a17a659b07e
Rob White <robert.emery1@gmail.com>
2020-04-06 23:22:17 -0400

    feat: tags and links n stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,37 @@
 const path = require('path')
 
+const createTagPages = (createPage, posts) => {
+  const singleTagIndexTemplate = path.resolve('src/templates/singleTagIndex.js')
+  const postsByTag = {}
+
+  posts.forEach(({ node }) => {
+    if (node.frontmatter.tags) {
+      node.frontmatter.tags.forEach(tag => {
+        if (!postsByTag[ tag ]) {
+          postsByTag[ tag ] = []
+        }
+
+        postsByTag[ tag ].push(node)
+      })
+    }
+  })
+
+  const tags = Object.keys(postsByTag)
+
+  tags.forEach(tagName => {
+    const post = postsByTag[ tagName ]
+
+    createPage({
+      path: `tags/${tagName}`,
+      component: singleTagIndexTemplate,
+      context: {
+        tagName,
+        posts: post
+      }
+    })
+  })
+}
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
 
@@ -10,11 +42,15 @@ exports.createPages = ({ graphql, actions }) => {
       graphql(
         `
           query {
-            allMarkdownRemark {
+            allMarkdownRemark(
+              sort: { order: DESC, fields: [frontmatter___date]}
+            ) {
               edges {
                 node {
                   frontmatter {
                     path
+                    tags
+                    title
                   }
                 }
               }
@@ -22,13 +58,19 @@ exports.createPages = ({ graphql, actions }) => {
           }
         `
       ).then(result => {
-        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        const posts = result.data.allMarkdownRemark.edges
+
+        createTagPages(createPage, posts)
+
+        posts.forEach(({ node }, idx) => {
           const path = node.frontmatter.path
           createPage({
             path,
             component: blogPostTemplate,
             context: {
-              pathSlug: path
+              pathSlug: path,
+              prev: idx === (posts.length - 1) ? null : posts[ idx + 1 ].node,
+              next: idx === 0 ? null : posts[ idx - 1 ].node,
             }
           })
         })
