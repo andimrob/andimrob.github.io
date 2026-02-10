c62dbf15f20049195415b949ad05650fbb6ab2b3
Rob Blakey <andimrob@gmail.com>
2026-02-10 23:59:59 +0000

    Revert "Add sparkler trail Easter egg on click-and-drag"
    This reverts commit 3a718afc8e3dcb36058381fe978cce7ca20083dc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useEffect } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import { initSparklerTrail } from "./sparklerTrail";
 
 function App() {
-  useEffect(() => initSparklerTrail(), []);
-
   return (
     <>
       <Header />
